caa54b4181095ee8a803a2ce8cfe4bce2d9957da
Austin Gabel <agabel@gmail.com>
2011-10-26 01:56:26 -0500

    Added tests for new methods

--- weathertests.py
+++ weathertests.py
@@ -1,13 +1,14 @@
 import unittest
-import weather
+from weather import weather
 
-KEY = ''
-LOCATION = ''
+KEY = 'af3292e8dba2ce2f'
+LOCATION = 'KS/Hays'
 
 class WeatherTestCase(unittest.TestCase):
 
     def setUp(self):
         self.client = weather.Client(key=KEY)
+
 
     def test_conditions(self):
         response = self.client.request(features=['conditions'], location=LOCATION)
@@ -19,7 +20,25 @@ class WeatherTestCase(unittest.TestCase):
         response = self.client.request(features=['conditions', 'forecast'], location=LOCATION)
         self.assertIsNotNone(response)
 
-        
+
+    def test_features_by_location(self):
+        response = self.client.get_features_by_city('Hays', 'KS', features=['conditions'])
+        self.assertIsNotNone(response)
+
+        response = self.client.get_features_by_city(city='Sidney', country='Australia', features=['conditions'])
+        self.assertIsNotNone(response)
+
+        try:
+            response = self.client.get_features_by_city(city='Wichita', features=['conditions'])
+            raise self.failureException
+        except weather.WeatherInvalidLocation:
+            self.assertIsNotNone(response)
+
+
+
+    def test_features_by_ip(self):
+        response = self.client.get_features_by_ip('24.225.1.168', features=['conditions'])
+        self.assertIsNotNone(response)
 
 
 if __name__ == "__main__":
